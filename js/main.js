0dc45d2a2c56ecbd12fe0a88b865f57a156e9392
Ilya Merkulov <77540282+IlyaM70@users.noreply.github.com>
2022-12-21 09:26:51 +0500

    Message

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
+// Dropdown
 var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
 var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
   return new bootstrap.Dropdown(dropdownToggleEl)
 })
+// Modile Click On Burger Start
+$(".burger").click(
+  function () {
+    if ($(".menu").hasClass('d-none')) {
+      $(".menu").removeClass("d-none");
+      $(".cart").addClass("d-none");
+      $(".catalog").addClass("w-100");
+    }
+    else{
+      $(".menu").addClass("d-none");
+      $(".cart").removeClass("d-none");
+      $(".catalog").removeClass("w-100");
+    }
+  }
+)
+// Modile Click On Burger End
