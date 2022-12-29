6c25aa273a7f8413b080707a874c39731145404f
IlyaM70 <iliya.merkulov@mail.ru>
2022-12-29 17:08:29 +0500

    message

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Dropdown
 var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
 var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
@@ -22,26 +21,3 @@ $(".burger").click(
   }
 )
 // Modile Click On Burger End
-=======
-// Dropdown
-var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
-var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
-  return new bootstrap.Dropdown(dropdownToggleEl)
-})
-// Modile Click On Burger Start
-$(".burger").click(
-  function () {
-    if ($(".menu").hasClass('d-none')) {
-      $(".menu").removeClass("d-none");
-      $(".cart").addClass("d-none");
-      $(".catalog").addClass("w-100");
-    }
-    else{
-      $(".menu").addClass("d-none");
-      $(".cart").removeClass("d-none");
-      $(".catalog").removeClass("w-100");
-    }
-  }
-)
-// Modile Click On Burger End
->>>>>>> 6530de1a6fc496fd44a1c40ec7e0c783186fdd5d
